d79caccbcb6cdd5d648ee7abaf6f2be3064d2403
shubham-khu <84306152+shubham-khu@users.noreply.github.com>
2023-01-24 15:37:04 +0530

    Update First.java

--- First.java
+++ First.java
@@ -2,5 +2,6 @@ class First {
     public static void main(String[] args) {
         System.out.println("Jai Siya Ram bhai!");
         System.out.println("Jai Mata di!");
+        System.out.println("Har Har Mahadev!");
     }
 }
